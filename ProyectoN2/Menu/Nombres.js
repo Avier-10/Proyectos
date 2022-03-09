61ebd5e15aabea90c15bc32ae439c520acf62074
Emilia Sosa Tomada <79819818+emiliasosa@users.noreply.github.com>
2022-03-08 22:40:07 -0300

    Update Nombres.js

--- ProyectoN2/Menu/Nombres.js
+++ ProyectoN2/Menu/Nombres.js
@@ -1,12 +1,12 @@
 // variables para pedir nombre de jugadores
-let jugador1 = prompt("Ingresa tu nombre jugador 1: ");
-let jugador2 = prompt("Ingresa tu nombre jugador 2: ");
+let player1 = prompt("Ingresa tu nombre jugador 1: ");
+let player2 = prompt("Ingresa tu nombre jugador 2: ");
 
   // variable para acceder al DOM de los nombres
   let nombre1 = document.createElement("p");
-  nombre1.innerHTML = jugador1;
+  nombre1.innerHTML = player1;
   document.getElementById("jugador1").appendChild(nombre1);
 
   let nombre2 = document.createElement("p");
-  nombre2.innerHTML = jugador2;
+  nombre2.innerHTML = player2;
   document.getElementById("jugador2").appendChild(nombre2);
