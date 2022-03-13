397322c589d842e1dc3a9b902fd372e3ba281881
Diana <diana__rh@hotmail.com>
2022-03-12 21:15:36 -0600

    se muestra en el Dom los nombres

--- ProyectoN2/menu/index.js
+++ ProyectoN2/menu/index.js
@@ -10,12 +10,19 @@ let nombre2 = prompt("Ingresa tu nombre jugador 2: ");
 const color1 = prompt("Jugador 1 elige el color de tu figura: verde, naranja, morado o azul ");
 const color2 = prompt("Jugador 2 elige el color de tu figura: verde, naranja, morado o azul ");
 
-
-
 const jugador1 = new Nombre(nombre1);
 const jugador2 = new Nombre(nombre2);
 
-// orden del menu
+// mostrar en el Dom 
+let name1 = document.createElement("p");
+name1.innerHTML = jugador1.nombre;
+document.getElementById("jugador1").appendChild(name1);
+
+let name2 = document.createElement("p");
+name2.innerHTML = jugador2.nombre;
+document.getElementById("jugador2").appendChild(name2);
+
+// mostrar en consola
 console.log(jugador1);
 console.log(jugador2);
 
