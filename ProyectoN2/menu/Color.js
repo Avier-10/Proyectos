28f1401acb6ae656f2c853c0ab02a242205bb397
Diana <diana__rh@hotmail.com>
2022-03-12 22:42:17 -0600

    Se agrega clase color

--- ProyectoN2/menu/Color.js
+++ ProyectoN2/menu/Color.js
@@ -1,25 +1,39 @@
-// variables
-const verde = "#1bd832"
-const verdeFondo = "#90ee90"
-const naranja = "#ff4500"
-const naranjaFondo = "#ff4500"
-const morado = "#800080"
-const moradoFondo = "#00a86b"
-const azul = "#20b2aa"
-const azulFondo = "#d19fe8"
-/*
-// frente
-  let colores = document.getElementsByClassName('fondo');
-  colores.style.backgroundColor = moradoFondo;
-// fondo*/
+   /* // variables
+    const verde = "#1bd832"
+    const verdeFondo = "#90ee90"
+    const naranja = "#ff4500"
+    const naranjaFondo = "#ff4500"
+    const morado = "#800080"
+    const moradoFondo = "#00a86b"
+    const azul = "#20b2aa"
+    const azulFondo = "#d19fe8"*/
 
 class Color{
   constructor(color){
-      this.color = color
+    this.color = color
     
   }
   mostrarColor(){
-      return `Hola ${this.color}`
+     // variables
+    const verde = "#1bd832"
+    const naranja = "#ff4500"
+    const morado = "#800080"
+    const azul = "#20b2aa"
+
+    switch (this.color) {
+      case "verde":
+       return verde
+        break;
+      case "naranja":
+        return naranja
+        break;
+      case "morado":
+        return morado
+        break;
+      case "azul":
+        return azul
+        break;
+    }
   }
 }
 
--- ProyectoN2/menu/index.js
+++ ProyectoN2/menu/index.js
@@ -10,19 +10,30 @@ let nombre2 = prompt("Ingresa tu nombre jugador 2: ");
 const color1 = prompt("Jugador 1 elige el color de tu figura: verde, naranja, morado o azul ");
 const color2 = prompt("Jugador 2 elige el color de tu figura: verde, naranja, morado o azul ");
 
+// ejecutar clases
 const jugador1 = new Nombre(nombre1);
 const jugador2 = new Nombre(nombre2);
+const colores1 = new Color(color1);
+const colores2 = new Color(color2);
 
 // mostrar en el Dom 
 let name1 = document.createElement("p");
-name1.innerHTML = jugador1.nombre;
+name1.innerHTML = jugador1.mostrarNombre();
 document.getElementById("jugador1").appendChild(name1);
 
 let name2 = document.createElement("p");
-name2.innerHTML = jugador2.nombre;
+name2.innerHTML = jugador2.mostrarNombre();
 document.getElementById("jugador2").appendChild(name2);
 
+let colors1 = document.getElementById('color1');
+colors1.style.backgroundColor = colores1.mostrarColor(); 
+
+let colors2 = document.getElementById('color2');
+colors2.style.backgroundColor = colores2.mostrarColor(); 
+
 // mostrar en consola
-console.log(jugador1);
-console.log(jugador2);
+console.log(jugador1.mostrarNombre());
+console.log(jugador2.mostrarNombre());
+console.log(colores1.mostrarColor());
+console.log(colores2.mostrarColor());
 
