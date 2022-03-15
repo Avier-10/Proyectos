9415062680f140358cea6dd2b8d9f6fa0fbe8cb6
Diana <diana__rh@hotmail.com>
2022-03-15 13:05:17 -0600

    seleccionar color se agrega

--- ProyectoN2/menu/index.js
+++ ProyectoN2/menu/index.js
@@ -27,6 +27,44 @@ document.getElementById("nombrecont2").appendChild(name2);
 // let colors2 = document.getElementById('color2');
 // colors2.style.backgroundColor = jugador2.mostrarColor(); 
 
+
+// color jugador 1
+const verde = "#1bd832"
+const naranja = "#ff4500"
+const morado = "#800080"
+const azul = "#20b2aa"
+
+const color = ["verde", "naranja", "morado", "azul"];
+
+const colores = document.querySelector("#radioColores");
+colores.innerHTML = color.map((colores) => 
+    `<div>
+      <input type="radio" name="colores" value="${colores}" id="${colores}">
+      <label for="${colores}">${colores}</label>
+    </div>`).join(' ');
+
+const radioButtons = document.querySelectorAll('input[name="colores"]');
+for(const radioButton of radioButtons){
+    radioButton.addEventListener('change', colorSeleccionado);
+} 
+
+
+function colorSeleccionado() {
+
+    if (this.checked) {
+        if("azul" === this.value){
+            console.log(azul)
+        }else if("verde" === this.value){
+            console.log(verde)
+        }else if("naranja" === this.value){
+            console.log(naranja)
+        }else if("morado" == this.value){
+            console.log(morado)
+        }
+    }
+}
+
+
 // mostrar en consola
 console.log(jugador1);
 console.log(jugador2);
