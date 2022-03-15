03b9cfa594e883f1fbd1ea0ebc45493c73be63b3
Emilia Sosa Tomada <79819818+emiliasosa@users.noreply.github.com>
2022-03-15 05:31:09 -0300

    seleccion de figura con verificaciones 1vs2

--- ProyectoN2/index.js
+++ ProyectoN2/index.js
@@ -22,18 +22,85 @@ const ganador = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]
 let selector = 0;
 
 let seleccion = (id) => {
-    id == "cir" ?  pl1 = "Figura_Circulo" : "";
-    id == "cua" ? pl1 = "Figura_Cuadrado" : "";
-    id == "tri" ? pl1 = "Figura_Triangulo" : "";
-    id == "eq" ?  pl1 = "Figura_X" : "";
+    if(id == "cir" && pl2 != "Figura_Circulo" ){
+        pl1 = "Figura_Circulo" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq").classList.remove ("contenedorFigurasClick")
+    } else if(id == "cir" && pl2 == "Figura_Circulo" ){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "cua" && pl2 != "Figura_Cuadrado" ){
+        pl1 = "Figura_Cuadrado" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cir").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq").classList.remove ("contenedorFigurasClick")
+    }else if(id == "cua" && pl2 == "Figura_Cuadrado" ){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "tri"  && pl2 != "Figura_Triangulo"){
+        pl1 = "Figura_Triangulo" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua").classList.remove ("contenedorFigurasClick")
+        document.getElementById("cir").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq").classList.remove ("contenedorFigurasClick")
+    }else if(id == "tri"  && pl2 == "Figura_Triangulo"){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "eq"  && pl2 != "Figura_X"){
+        pl1 = "Figura_X" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri").classList.remove ("contenedorFigurasClick")
+        document.getElementById("cir").classList.remove ("contenedorFigurasClick")
+    } else if(id == "eq"  && pl2 == "Figura_X"){
+        alert("Selecciona otra figura!")
+    }
+    
 };
 
 let seleccion2 = (id) => {
-    id == "cir" ?  pl2 = "Figura_Circulo" : "";
-    id == "cua" ? pl2 = "Figura_Cuadrado" : "";
-    id == "tri" ? pl2 = "Figura_Triangulo" : "";
-    id == "eq" ?  pl2 = "Figura_X" : "";
+    if(id == "cir2" && pl1 != "Figura_Circulo"){
+        pl2 = "Figura_Circulo" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq2").classList.remove ("contenedorFigurasClick")
+    }else if(id == "cir2" && pl1 == "Figura_Circulo" ){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "cua2" && pl1 != "Figura_Cuadrado"){
+        pl2 = "Figura_Cuadrado" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cir2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq2").classList.remove ("contenedorFigurasClick")
+    }else if(id == "cua2" && pl1 == "Figura_Cuadrado" ){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "tri2" && pl1 != "Figura_Triangulo"){
+        pl2 = "Figura_Triangulo" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("cir2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("eq2").classList.remove ("contenedorFigurasClick")
+    }else if(id == "tri2"  && pl1 == "Figura_Triangulo"){
+        alert("Selecciona otra figura!")
+    }
+    if(id == "eq2" && pl1 != "Figura_X"){
+        pl2 = "Figura_X" 
+        document.getElementById(id).classList.add ("contenedorFigurasClick")
+        document.getElementById("cua2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("tri2").classList.remove ("contenedorFigurasClick")
+        document.getElementById("cir2").classList.remove ("contenedorFigurasClick")
+    }else if(id == "eq2"  && pl1 == "Figura_X"){
+        alert("Selecciona otra figura!")
+    }
 };
+
+let pl2;
 
 
 /*
