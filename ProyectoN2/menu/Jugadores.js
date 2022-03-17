874bd360f972e90c078047c6c3da85a3e39a5de0
Diana <diana__rh@hotmail.com>
2022-03-16 22:39:21 -0600

    elimina código comentado

--- ProyectoN2/menu/Jugadores.js
+++ ProyectoN2/menu/Jugadores.js
@@ -1,13 +1,3 @@
-/* // variables se van a usar cuando se una al tablero
-const verde = "#1bd832"
-const verdeFondo = "#90ee90"
-const naranja = "#ff4500"
-const naranjaFondo = "#ff4500"
-const morado = "#800080"
-const moradoFondo = "#00a86b"
-const azul = "#20b2aa"
-const azulFondo = "#d19fe8"*/
-
 class Jugadores{
     constructor(nombre,color, figura){
         this.nombre = nombre
@@ -17,27 +7,6 @@ class Jugadores{
     mostrarNombre(){
         return `Bienvenid@ <b>${this.nombre}</b>`
     }
-    mostrarColor(){
-        // variables
-        const verde = "#1bd832"
-        const naranja = "#ff4500"
-        const morado = "#800080"
-        const azul = "#20b2aa"
-        
-        switch (this.color) {
-            case "verde":
-                return verde
-                break;
-            case "naranja":
-                return naranja
-                break;
-            case "morado":
-                return morado
-                break;
-            case "azul":
-                return azul
-                break;
-            }
-        }
-    }
+
+}
 
--- ProyectoN2/menu/index.js
+++ ProyectoN2/menu/index.js
@@ -12,15 +12,6 @@ while(nombre2 == "" ){
     nombre2 = prompt("Ingresa tu nombre jugador 2: ");
 }
 
-// nombre de jugadores
-//let nombre1 = prompt("Ingresa tu nombre jugador 1: ");
-//let nombre2 = prompt("Ingresa tu nombre jugador 2: ");
-
-
-// color de figuras
-//const color1 = prompt("Jugador 1 elige el color de tu figura: verde, naranja, morado o azul ");
-// const color2 = prompt("Jugador 2 elige el color de tu figura: verde, naranja, morado o azul ");
-
 // ejecutar clase
 const jugador1 = new Jugadores(nombre1);
 const jugador2 = new Jugadores(nombre2);
@@ -35,16 +26,7 @@ let name2 = document.createElement("p");
 name2.innerHTML = jugador2.mostrarNombre();
 document.getElementById("nombrecont2").appendChild(name2);
 
-// let colors1 = document.getElementById('color1');
-// colors1.style.backgroundColor = jugador1.mostrarColor(); 
-
-// let colors2 = document.getElementById('color2');
-// colors2.style.backgroundColor = jugador2.mostrarColor(); 
-
-
 // color jugador 1
-
-
 const color = ["verde", "naranja", "morado", "azul"];
 
 const colores = document.querySelector("#radioColores");
@@ -81,7 +63,6 @@ function colorSeleccionado() {
 }
 
 // color jugador 2
-
 const color2 = ["verde", "naranja", "morado", "azul"];
 
 const colores2 = document.querySelector("#radioColores2");
@@ -95,7 +76,6 @@ const radioButtons2 = document.querySelectorAll('input[name="colores2"]');
 for(const radioButton2 of radioButtons2){
     radioButton2.addEventListener('change', colorSeleccionado2);
 } 
-
 
 function colorSeleccionado2() {
     const verde = "#1bd832"
